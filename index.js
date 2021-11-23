604775deeba3682c6ed88ea4afefc2d672e2a4e3
Jeffery McNeil <jeffmc825@gmail.com>
2021-11-23 17:13:54 -0600

    commit tree 9ee40b42db6706ee778a9a077f0d934fea91740b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function ([a, b, c, d]) {
+    let array = [a, b, c, d]
+    let array2 = array.slice(0,2)
+    return array2
+}
+
+const returnLastTwoDrivers = function ([a, b, c, d]) {
+    let array = [a, b, c, d]
+    let array2 = array.slice(2,4)
+    return array2
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier (x) {
+    const multiply = x => x**2;
+    return multiply; 
+}
+
+function fareDoubler(x) {
+    const multiply = x;
+    return multiply*2
+}
+
+function fareTripler(x) {
+    const multiply = x;
+    return multiply*3
+}
+
+function selectDifferentDrivers([a, b, c, d], functionChoice)
+{   
+   switch (functionChoice) {
+       case returnFirstTwoDrivers: 
+           return returnFirstTwoDrivers([a, b, c, d]);
+       case returnLastTwoDrivers:
+           return returnLastTwoDrivers([a, b, c, d]);
+
+   };
+}
